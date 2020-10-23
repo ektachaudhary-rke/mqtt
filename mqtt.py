34b02b8560917ff4347ffd2f9929f8a530852171
Ekta Chaudhary <60789158+ektachaudhary-rke@users.noreply.github.com>
2020-10-23 14:27:18 +0530

    Add files via upload

---
+++ mqtt.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt 
+import time
+
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+
+broker_address="test.mosquitto.org"
+print("creating new instance")
+client = mqtt.Client("P1") #create new instance
+client.on_message=on_message #attach function to callback
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+client.loop_start() #start the loop
+print("Subscribing to topic","test/topic")
+client.subscribe("test/topic")
+print("Publishing message to topic","test/topic")
+client.publish("test/topic","OFF")
+time.sleep(4) # wait
+client.loop_stop() #stop the loop
